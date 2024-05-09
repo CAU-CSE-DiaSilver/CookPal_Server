71488abb74c5fd05fcd048d361d0e0b576977ade
YounDaHee <dbsekgml518@gmail.com>
2024-05-10 08:34:58 +0900

    not found error

--- crawling/search_list.js
+++ crawling/search_list.js
@@ -5,11 +5,12 @@ const spawn = require('child_process').spawn;
 
 router.post('/', async(req, res)=>{
     let search = req.body.recipe_name;
-    console.log("commit_test")
     console.log(search)
     if(search!=undefined){
+        console.log("commit_test1")
         const racipe_list = spawn('python3', ['./crawling/search_list.py', search]);
         racipe_list.stdout.on('data', async(result)=>{
+            console.log("commit_test2")
             let result_list = result.toString()
             console.log(result_list)
             result_list = result_list.replace(/'/g, '"')
