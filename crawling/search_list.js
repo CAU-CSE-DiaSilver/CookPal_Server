18a78b2efcce2d7ff6bb0f94cefbd995752cce06
YounDaHee <dbsekgml518@gmail.com>
2024-05-10 08:43:25 +0900

    not found error

--- crawling/search_list.js
+++ crawling/search_list.js
@@ -8,7 +8,7 @@ router.post('/', (req, res)=>{
     console.log(search)
     if(search!=undefined){
         const racipe_list = spawn('python3', ['./crawling/search_list.py', search]);
-        racipe_list.stdout.on('data', async(result)=>{
+        racipe_list.stdout.on('data', (result)=>{
             console.log(result.toString())
             let result_list = result.toString()
             result_list = result_list.replace(/'/g, '"')
