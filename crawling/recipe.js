dafd21fe93292032e5e76dd3e9bee932955020d9
YounDaHee <dbsekgml518@gmail.com>
2024-05-10 13:39:50 +0900

    ver1

--- crawling/recipe.js
+++ crawling/recipe.js
@@ -5,17 +5,16 @@ const spawn = require('child_process').spawn;
 
 router.post('/', (req, res)=>{
     let recipe_url = req.body.recipe_url;
-    console.log(recipe_url)
     if(recipe_url!=undefined){
         const racipe_info = spawn('python3', ['./crawling/recipe_get.py', recipe_url]);
-        console.log("test2")
-        racipe_info.stdout.on('data', (result)=>{
-            console.log("test1")
-            let result_recipe = result.toString()
-            console.log(result_recipe)
-            result_recipe = result_recipe.replace(/'/g, '"')
-            res.json(JSON.parse(result_recipe))
-        });
+        try{
+            racipe_info.stdout.on('data', (result)=>{
+                let result_recipe = result.toString()
+                result_recipe = result_recipe.replace(/'/g, '"')
+                res.json(JSON.parse(result_recipe))
+                console.log(recipe_url)
+            });
+        }catch(error){}
     }
     
 });
--- crawling/search_list.js
+++ crawling/search_list.js
@@ -5,15 +5,14 @@ const spawn = require('child_process').spawn;
 
 router.post('/', (req, res)=>{
     let search = req.body.recipe_name;
-    console.log(search)
     if(search!=undefined){
         const racipe_list = spawn('python3', ['./crawling/search_list.py', search]);
         racipe_list.stdout.on('data', (result)=>{
             try{
                 let result_list = result.toString()
                 result_list = result_list.replace(/'/g, '"')
-                console.log(result_list)
                 res.json(JSON.parse(result_list))
+                console.log(search)
             }catch(error){}
         });
     }
