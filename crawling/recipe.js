3bc7b276956413ec3530ea68f94a65371c1d8f12
YounDaHee <dbsekgml518@gmail.com>
2024-05-10 14:02:21 +0900

    ver1

--- crawling/recipe.js
+++ crawling/recipe.js
@@ -9,10 +9,11 @@ router.post('/', (req, res)=>{
         const racipe_info = spawn('python3', ['./crawling/recipe_get.py', recipe_url]);
         try{
             racipe_info.stdout.on('data', (result)=>{
+                console.log(recipe_url+"start")
                 let result_recipe = result.toString()
                 result_recipe = result_recipe.replace(/'/g, '"')
                 res.json(JSON.parse(result_recipe))
-                console.log(recipe_url)
+                console.log(recipe_url+"finish")
             });
         }catch(error){}
     }
