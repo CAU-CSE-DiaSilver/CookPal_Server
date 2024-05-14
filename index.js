759b9132010be404d3b98afeaf027b5ac62aceff
YounDaHee <dbsekgml518@gmail.com>
2024-05-14 13:33:42 +0900

    notclose

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ app.use(express.urlencoded({extended :false}));
 app.use('/search_list', search_list);
 app.use('/recipe', recipe);
 
+app.get('/healthz', (req, res) => {
+    res.send('Server On');
+})
+
 app.use((req, res, next)=>{
     res.status(404).send('404 Not Found');
 });
