8db1dcb8adc50ddc19f29db8331b72bbf9230c1d
YounDaHee <dbsekgml518@gmail.com>
2024-05-24 12:29:03 +0900

    remove not unicode

--- crawling/recipe_get.py
+++ crawling/recipe_get.py
@@ -3,16 +3,10 @@ import io
 import json
 import requests
 from bs4 import BeautifulSoup
-import unicodedata
 
 # 한글 출력을 위한 설정
 sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding='utf-8')
 sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding='utf-8')
-
-def clean_json_string(json_string):
-    # 유니코드가 아닌 문자를 제거하거나 대체
-    cleaned_string = ''.join(ch if unicodedata.category(ch)[0] != 'C' else ' ' for ch in json_string)
-    return cleaned_string
 
 def recipe_get(recipe) :
     link = f"https://www.10000recipe.com{recipe}"
@@ -27,8 +21,8 @@ def recipe_get(recipe) :
             
             json_data = json.loads(script.string)
             if json_data["@type"] == "Recipe":
-                recipe_out["name"] = clean_json_string(json_data["name"].replace("'", "\*").replace("\"", "\*"))
-                recipe_out["description"] = clean_json_string(json_data["description"].replace("'", "\*").replace("\"", "\*"))
+                recipe_out["name"] = json_data["name"].replace("'", "\*").replace("\"", "\*").replace(u'\xa0', u'')
+                recipe_out["description"] = json_data["description"].replace("'", "\*").replace("\"", "\*").replace(u'\xa0', u'')
                 recipe_out["image"] = json_data["image"]
                 recipe_out["totalTime"] = json_data["totalTime"]
                 recipe_out["recipeYield"] = json_data["recipeYield"]
@@ -43,8 +37,8 @@ def recipe_get(recipe) :
                 recipe_list = json_data["recipeInstructions"]
                 for step in recipe_list :
                     if step["@type"] == "HowToStep":
-                        recipe_out["recipe_list"].append({"step" : clean_json_string(step["text"].replace("'", "\*").replace("\"", "\*")), "image" : step["image"]})
-                        recipe_out["recipe_text"].append(clean_json_string(step["text"].replace("'", "\*").replace("\"", "\*")))
+                        recipe_out["recipe_list"].append({"step" : step["text"].replace("'", "\*").replace("\"", "\*").replace(u'\xa0', u''), "image" : step["image"]})
+                        recipe_out["recipe_text"].append(step["text"].replace("'", "\*").replace("\"", "\*").replace(u'\xa0', u''))
                 
     print(recipe_out)
 
